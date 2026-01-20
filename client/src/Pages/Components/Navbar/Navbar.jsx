ef6e763dd882223c487cba4b4f46511368db7fab
Srikanth Reddy <93464981+srikstar@users.noreply.github.com>
2026-01-20 17:29:51 +0530

    Enhance Navbar with icons and sub-navigation
    Added profile and shopping cart icons to the Navbar, including a cart item count overlay. Introduced a sub-navigation bar with category links and updated related CSS for improved layout and responsiveness. Also fixed a typo in index.css and adjusted main container padding.

--- client/src/Pages/Components/Navbar/Navbar.jsx
+++ client/src/Pages/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './Navbar.css'
 
@@ -10,10 +10,30 @@ function Navbar() {
         <div className="nav-content-container">
           <Link to='/'><img className='donut-logo' src="./donut.jpg" alt="donut-logo" /></Link>
         </div>
-        <div className="nav-content-container"></div>
+        <div className="nav-content-container row">
+          <Link to='/'><img src="./profile.png" alt="profile-icon" className="profile-icon nav-icons" /></Link>
+          <div className="overlay-number">
+            <Link to='/'><span className='cart-content-count'>5</span></Link>
+            <Link to='/'><img src="./shopping-cart.png" alt="shopping-cart" className="shopping-cart nav-icons" /></Link>
+          </div>
+        </div>
+      </div>
+      <div className="sub-navbar-main-container row">
+        <div className="subnav-max row">
+          <div className="sub-navbar row-sb">
+            <Link className="sub-navlinks">Sweets</Link>
+            <Link className="sub-navlinks">Namkeens</Link>
+            <Link className="sub-navlinks">Snacks</Link>
+            <Link className="sub-navlinks">Boulangerie</Link>
+            <Link className="sub-navlinks">Chocolates</Link>
+            <Link className="sub-navlinks">Gifting</Link>
+            <Link className="sub-navlinks">Candles</Link>
+          </div>
+        </div>
       </div>
     </section>
   )
 }
 
 export default Navbar
+
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <>
       <section className='main-container'>
-        <h1>Home</h1>
+        
       </section>
     </>
   )
